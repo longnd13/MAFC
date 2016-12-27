0a6a1b8ce681ab77cc4a013359c216dad1f01c98
longnd13 <ndlong13@gmail.com>
2016-12-27 11:00:07 +0700

    update

---
+++ Prj.Respositories/Entity/DepartmentEntity.cs
@@ -0,0 +1,20 @@
+﻿using PetaPoco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prj.Respositories.Entity
+{
+    [TableName("Department")]
+    [PrimaryKey("Id", autoIncrement = true)] // thêm khóa chính để câu lệnh insert trả về ID hiện có của User
+    public class DepartmentEntity : LogsDateEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+
+}
---
+++ Prj.Respositories/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PetaPoco;
+namespace Prj.Respositories.UnitOfWork
+{
+  public  interface IUnitOfWork : IDisposable
+    {
+        void Commit();
+        Database Db { get; }
+    }
+}
