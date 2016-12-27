c80e2ec2ee1a4a320724deb29f3ccf9a4a420a4c
longnd13 <ndlong13@gmail.com>
2016-12-27 15:26:28 +0700

    ok

---
+++ Prj.Web/Areas/Admin/Controllers/GroupPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prj.Web.Areas.Admin.Controllers
+{
+    public class GroupPageController : Controller
+    {
+        // GET: Admin/GroupPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
